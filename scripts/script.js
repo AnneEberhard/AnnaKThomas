27d7b9f185de4b3129f3642755753ab677a0161b
AnneEberhard <anne.eberhard@gmx.net>
2023-11-19 10:43:48 +0100

    general layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,25 @@
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-          if (this.readyState == 4) {
-            if (this.status == 200) {
-              elmnt.innerHTML = this.responseText;
-            }
-            if (this.status == 404) {
-              elmnt.innerHTML = "Page not found.";
-            }
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-          }
-        };
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        return;
-      }
-    }
+let browserLanguage = navigator.language || navigator.userLanguage;
+let defaultLanguage = "en";
+
+function init() {
+  checkBrowserLanguage();
+}
+
+function checkBrowserLanguage() {
+  if (browserLanguage.startsWith('de')) {
+      defaultLanguage = 'de';
+      german();
+  } else {
+      defaultLanguage = 'en';
+      english();
   }
+}
+
+
+function german() {
+  //switch text to german
+}
+
+function english() {
+  //switch text to english
+}
